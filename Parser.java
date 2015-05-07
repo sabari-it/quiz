091fd1c5db90350d12f8d180a8d14f9abbe4bb4e
sabari-it <sunthara.Kirubahar@gmail.com>
2015-05-07 11:47:30 +0100

    Update Parser.java
    I am a expert in C++ and the observation in this file as put a comment on the top of the file.

--- Parser.java
+++ Parser.java
@@ -1,3 +1,26 @@
+/* I am a c++ and observerd the following 
+
+file is not properly guarded. The getter/setter are synchronized, but no other access to the field file is synchronized. 
+Not a massive issue at this point, but it should be better. Add more mutator methods and it starts becoming a big issue.
+
+If file needs to be thread safe it should be a volatile variable (so it publishes across core/cache lines on change). 
+Individual access to it should be properly synchronized.
+
+String appending should be replaced by a StringBuilder that does a toString() at the end. 
+The JVM will do this for you automatically, but not as efficiently as you want. You should not be doing
+
+  String myString = "";
+  myString += "hello";  // Don't put this in a loop
+
+Input and output streams are never closed. Resource leak.
+
+I'd put on a default constructor. But that's just me.
+
+I'd probably read and write in blocks rather than individual characters.
+
+There is duplicate code in the getContent()/getContentWithoutUnicode() which could be extracted ... not sure how I'd work it, but it irks me.
+
+*/
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
